c251ab0f6c55abd1184c620050d539314219d6ac
gcarreon17 <gcarreon@vertere-gs.com>
2025-07-28 19:47:13 +0800

    updated loginpage.cy.js
    - added before block and removed cy.signup() in all it blocks

---
+++ cypress/e2e/UI-AutomationExercise/loginPage.cy.js
@@ -0,0 +1,41 @@
+///<reference types="cypress" />
+
+describe("Blank Mandatory Fields in Login Page", { testIsolation: false }, () => {
+
+  before(() => {
+      cy.clearAllCookies();
+      cy.visit("https://www.automationexercise.com/");
+    });
+
+
+  it("Verify Email Address and Password fields in Login Page left blank display an error message", () => {
+    cy.get('.shop-menu > .nav > :nth-child(4) > a').click();
+    cy.get('[data-qa="login-button"]').click();
+    cy.get('[data-qa="login-email"], [data-qa="login-password"]').each(($input) => {
+      expect($input[0].checkValidity()).to.be.false;
+      cy.log($input[0].validationMessage);
+      });
+    cy.captureSnapshot("Automation Exercise/Verified email and password fields in Login Page left blank displayed an error message");
+   });
+
+
+  it("Verify email field in Login Page left blank displays an error message", () => {
+    cy.get('.shop-menu > .nav > :nth-child(4) > a').click();
+    cy.get('[data-qa="login-button"]').click();
+    cy.get('[data-qa="login-email"]').then(($input) => {
+      expect($input[0].checkValidity()).to.be.false;
+      cy.log($input[0].validationMessage); 
+    cy.captureSnapshot("Automation Exercise/Verified email field in SignUp Page left blank displayed an error message");
+   });
+  });
+
+
+  it("Verify password field in Login Page left blank displays an error message", () => {
+    cy.get('[data-qa="login-button"]').click();
+    cy.get('[data-qa="login-password"]').then(($input) => {
+      expect($input[0].checkValidity()).to.be.false;
+      cy.log($input[0].validationMessage); 
+    cy.captureSnapshot("Automation Exercise/Verified password field in SignUp Page left blank displayed an error message");
+   });
+  });
+});
